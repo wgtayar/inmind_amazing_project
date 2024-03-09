70b6373530d9d88f7a9269eea0e7be8da04ede3a
Wajih-Georges El Tayar <wgtayar@gmail.com>
2024-03-09 21:48:25 +0200

    Data loading error fixed in LoadingBMWDataset.py

--- src/LoadingBMWDataset.py
+++ src/LoadingBMWDataset.py
@@ -1,61 +1,72 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
-from PIL import Image, ImageDraw
 import os
 import json
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_image
+from torchvision.transforms import Compose, ToTensor
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-class LoadingBMWDataset(Dataset):
-    # Constructor, initialize the paths and transform 
-    def __init__(self, images_dir, labels_dir, transform=None):
-        self.images_dir = images_dir
-        self.labels_dir = labels_dir
+class ObjectDetectionDataset(Dataset):
+    def __init__(self, img_dir, annotation_dir, transform=None):
+        self.img_dir = img_dir
+        self.annotation_dir = annotation_dir
         self.transform = transform
-        self.images = [f for f in os.listdir(images_dir) if f.endswith('.jpg')]
-    
-    # Get the length of the dataset
+        self.img_names = [img_name for img_name in os.listdir(img_dir) if img_name.endswith('.jpg')]
+
     def __len__(self):
-        return len(self.images)
-    
-    # Get the sample
+        return len(self.img_names)
+
     def __getitem__(self, idx):
-        img_path = os.path.join(self.images_dir, self.images[idx])
-        label_path = os.path.join(self.labels_dir, self.images[idx].replace('.jpg', '.json'))
-        
-        # Load image
-        image = Image.open(img_path).convert("RGB")
-        
-        # Load labels
-        with open(label_path) as f:
+        img_path = os.path.join(self.img_dir, self.img_names[idx])
+        img = read_image(img_path).float() / 255.0  # Normalize to [0, 1]
+        annotation_file = os.path.join(self.annotation_dir, self.img_names[idx].replace('.jpg', '.json'))
+        with open(annotation_file) as f:
             annotations = json.load(f)
-        
-        # Transform the sample to a dictionary containing the image and labels
-        sample = {'image': image, 'annotations': annotations}
+        boxes = []
+        labels = []
+        class_names = []
+        for annotation in annotations:
+            boxes.append([annotation['Left'], annotation['Top'], annotation['Right'], annotation['Bottom']])
+            labels.append(annotation['ObjectClassId'])
+            class_names.append(annotation['ObjectClassName'])
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64)
         if self.transform:
-            sample = self.transform(sample)
-        
-        return sample
+            img = self.transform(img)
+        return img, boxes, labels, class_names
 
-# Function to visualize one image along with the bounding boxes and labels
-def visualize_image(sample, idx):
-    image, annotations = sample['image'], sample['annotations']
-    draw = ImageDraw.Draw(image)
-    for annotation in annotations:
-        # The bounding box coordinates
-        bbox = [annotation['Left'], annotation['Top'], annotation['Right'], annotation['Bottom']]
-        draw.rectangle(bbox, outline="red", width=3)
-        draw.text((bbox[0], bbox[1]), annotation['ObjectClassName'], fill="white")
-    image.show()
 
-# Loading images and labels from the directory
-images_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/images'
-labels_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/labels/json'
-dataset = LoadingBMWDataset(images_dir, labels_dir)
+img_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/images'
+annotations_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/labels/json'
 
-# Function showing 5 pictures
-def visualize_images(dataset, num_images=5):
-    for i in range(num_images):
-        sample = dataset[i]  # Get the ith sample
-        visualize_image(sample, i)
+# transform = Compose([ToTensor()])
+transform = None
+train_dataset = ObjectDetectionDataset(img_dir, annotations_dir, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
-# Execution of the above functions to visualize the images and labels
-visualize_images(dataset, num_images=5)
+
+def visualize_samples(data_loader, num_samples=5):
+    visualized_samples = 0
+    for images, boxes, labels, class_names in data_loader:
+        for i in range(images.size(0)):
+            if visualized_samples >= num_samples:
+                break
+            visualize_sample(images[i], boxes[i], labels[i], class_names[i])
+            visualized_samples += 1
+        if visualized_samples >= num_samples:
+            break
+
+def visualize_sample(img, boxes, labels, class_names):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(img.permute(1, 2, 0))
+    ax = plt.gca()
+    for box, class_name in zip(boxes, class_names):
+        # Draw rectangle around object
+        rect = patches.Rectangle((box[0], box[1]), box[2]-box[0], box[3]-box[1], linewidth=2, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        ax.text(box[0], box[1], class_name, color='white', fontsize=12, bbox=dict(facecolor='red', alpha=0.5))
+    plt.show()
+
+# Now, call the correct function with your DataLoader
+visualize_samples(train_loader, 5)
