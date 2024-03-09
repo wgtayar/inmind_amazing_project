c4014ad914fe1dd5e56b69ca15aa3336d34ae00b
Wajih-Georges El Tayar <wgtayar@gmail.com>
2024-03-09 23:46:46 +0200

    Data splitting for training and validation in ModelTraining.ipynb. LoadingBMWDataset modified to return correct YOLOV7 format. Previous implementation returning the labeled images commented out, not deleted.

--- src/LoadingBMWDataset.py
+++ src/LoadingBMWDataset.py
@@ -8,15 +8,21 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 class ObjectDetectionDataset(Dataset):
-    def __init__(self, img_dir, annotation_dir, transform=None):
+    def __init__(self, img_dir, annotation_dir, transform=None, img_names=None):
         self.img_dir = img_dir
         self.annotation_dir = annotation_dir
         self.transform = transform
-        self.img_names = [img_name for img_name in os.listdir(img_dir) if img_name.endswith('.jpg')]
+        if img_names is None:
+            self.img_names = [img_name for img_name in os.listdir(img_dir) if img_name.endswith('.jpg')]
+        else:
+            self.img_names = img_names
+        
 
     def __len__(self):
         return len(self.img_names)
-
+    
+    '''
+    # Use this version of the function when data visualization is desired
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_names[idx])
         img = read_image(img_path).float() / 255.0  # Normalize to [0, 1]
@@ -34,12 +40,45 @@ class ObjectDetectionDataset(Dataset):
         labels = torch.tensor(labels, dtype=torch.int64)
         if self.transform:
             img = self.transform(img)
-        return img, boxes, labels, class_names  # Return class names as well
+        return img, boxes, labels, class_names
+    '''
+    
+    # Use this version of the function when there is a need to use a YOLOV7 model
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_names[idx])
+        img = read_image(img_path).float() / 255.0
+        annotation_file = os.path.join(self.annotation_dir, self.img_names[idx].replace('.jpg', '.txt'))
+        
+        boxes = []
+        labels = []
+        with open(annotation_file) as f:
+            for line in f.readlines():
+                class_id, x_center, y_center, width, height = map(float, line.split())
+                labels.append(int(class_id))
+                # Convert YOLO format back to [left, top, right, bottom]
+                half_width, half_height = width / 2, height / 2
+                left = x_center - half_width
+                top = y_center - half_height
+                right = x_center + half_width
+                bottom = y_center + half_height
+                boxes.append([left, top, right, bottom])
+                
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64)
+        
+        if self.transform:
+            img = self.transform(img)
+        
+        return img, boxes, labels
 
 
 
 img_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/images'
-annotations_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/labels/json'
+# For labels when loading the data from the JSON files
+#annotations_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/labels/json'
+
+# For labels when loading the data from the txt files
+annotations_dir = '/home/wgt/Desktop/InMind Academy/AI_Track/Amazing_Project/inmind_amazing_project/data/Training/labels/yolo'
 
 # transform = Compose([ToTensor()])
 transform = None
@@ -69,5 +108,5 @@ def visualize_samples(data_loader, num_samples=5):
             break
 
 
-# Now, call the correct function with your DataLoader
-visualize_samples(train_loader, 5)
+# Visualizing the data, to be used with the first version of the __getitem__ function
+# visualize_samples(train_loader, 5)
